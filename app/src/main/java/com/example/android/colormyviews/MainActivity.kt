4bf025d2bd0a249ff3182fa17b83f1d25617ea6b
Ben Ross <benjaminjackross@gmail.com>
2022-05-26 16:28:24 +0100

    Lesson 2: Layouts - Conceptt 29: Exercise - Add Baseline Constraint and Button Chain

--- app/src/main/java/com/example/android/colormyviews/MainActivity.kt
+++ app/src/main/java/com/example/android/colormyviews/MainActivity.kt
@@ -32,7 +32,7 @@ class MainActivity : AppCompatActivity() {
 
     private fun setListeners() {
         // create a list of views (boxes + the container of the boxes - the constraint layout)
-        val views = listOf<View>(box_one_text, box_two_text, box_three_text, box_four_text, box_five_text, constraint_layout)
+        val views = listOf<View>(box_one_text, box_two_text, box_three_text, box_four_text, box_five_text, constraint_layout, red_button, green_button, yellow_button)
 
         // Go through each one and add the same click listener (the one they've provided for you to copy and paste)
         views.forEach { view ->
@@ -51,6 +51,11 @@ class MainActivity : AppCompatActivity() {
             R.id.box_three_text -> view.setBackgroundResource(android.R.color.holo_green_light)
             R.id.box_four_text -> view.setBackgroundResource(android.R.color.holo_green_dark)
             R.id.box_five_text -> view.setBackgroundResource(android.R.color.holo_green_light)
+
+            // Boxes using custom colors for background
+            R.id.red_button -> box_three_text.setBackgroundResource(R.color.my_red)
+            R.id.yellow_button -> box_four_text.setBackgroundResource(R.color.my_yellow)
+            R.id.green_button -> box_five_text.setBackgroundResource(R.color.my_green)
 
             else -> view.setBackgroundColor(Color.LTGRAY)
         }
